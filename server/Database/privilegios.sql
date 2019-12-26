273b2242b7819fa89b5c0e3495961833fd21363d
Juan Espinoza <jcespinoza13@gmail.com>
2019-12-26 15:31:54 -0600

    Versión Final

--- server/Database/privilegios.sql
+++
@@ -1,7 +0,0 @@
-GRANT USAGE ON *.* TO `nextu`@`localhost`;
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON `ef\_nextu\_db`.* TO `nextu`@`localhost`;
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON `ef_nextu_db`.`eventos` TO `nextu`@`localhost`;
-
-GRANT SELECT (`password`, `usuario`), INSERT, UPDATE (`fechaNac`, `nombre`, `password`, `usuario`) ON `ef_nextu_db`.`usuarios` TO `nextu`@`localhost`;
